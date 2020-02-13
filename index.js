cf1fc01730a39ff067e8890b8c3a44cada633e61
Brandon Pittman <brandon@brandonpittman.net>
2020-02-13 10:32:08 +0900

    Reset package

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-const RSS = require('rss')
-const fs = require('fs')
-const path = require('path')
+const RSS = require('rss');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function (api, options) {
-  api.beforeBuild(({ store }) => {
-    const feed = new RSS(options.feedOptions)
-    const { collection } = store.getContentType(options.contentTypeName)
-    const dateField = options.dateField || 'date'
+	api.beforeBuild(({store}) => {
+		const feed = new RSS(options.feedOptions);
+		const {collection} = store.getContentType(options.contentTypeName);
+		const dateField = options.dateField || 'date';
 
-    let collectionData = [...collection.data]
+		let collectionData = [...collection.data];
 
-    const collectionWithValidDates = collectionData.filter(node => !isNaN(new Date(node[dateField]).getTime()))
-    if (collectionWithValidDates.length === collectionData.length)
-      collectionData.sort((nodeA, nodeB) => {
-        if (options.latest) {
-          return new Date(nodeB[dateField]).getTime() - new Date(nodeA[dateField]).getTime()
-        } else {
-          return new Date(nodeA[dateField]).getTime() - new Date(nodeB[dateField]).getTime()
-        }
-      })
+		const collectionWithValidDates = collectionData.filter(node => !isNaN(new Date(node[dateField]).getTime()));
+		if (collectionWithValidDates.length === collectionData.length) {
+			collectionData.sort((nodeA, nodeB) => {
+				if (options.latest) {
+					return new Date(nodeB[dateField]).getTime() - new Date(nodeA[dateField]).getTime();
+				}
 
-    if (options.maxItems) {
-      collectionData = collectionData.filter((item, index) => index < options.maxItems)
-    }
+				return new Date(nodeA[dateField]).getTime() - new Date(nodeB[dateField]).getTime();
+			});
+		}
 
-    collectionData.forEach(item => {
-      feed.item(options.feedItemOptions(item))
-    })
+		if (options.maxItems) {
+			collectionData = collectionData.filter((item, index) => index < options.maxItems);
+		}
 
-    const output = {
-      dir: './static',
-      name: 'rss.xml',
-      ...options.output
-    }
+		collectionData.forEach(item => {
+			feed.item(options.feedItemOptions(item));
+		});
 
-    const outputPath = path.resolve(process.cwd(), output.dir)
-    const outputPathExists = fs.existsSync(outputPath)
-    const fileName = output.name.endsWith('.xml')
-      ? output.name
-      : `${output.name}.xml`
+		const output = {
+			dir: './static',
+			name: 'rss.xml',
+			...options.output
+		};
 
-    if (outputPathExists) {
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml())
-    } else {
-      fs.mkdirSync(outputPath)
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml())
-    }
-  })
-}
+		const outputPath = path.resolve(process.cwd(), output.dir);
+		const outputPathExists = fs.existsSync(outputPath);
+		const fileName = output.name.endsWith('.xml') ?
+			output.name :
+			`${output.name}.xml`;
+
+		if (outputPathExists) {
+			fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml());
+		} else {
+			fs.mkdirSync(outputPath);
+			fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml());
+		}
+	});
+};
