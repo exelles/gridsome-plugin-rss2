50a22e23a09ed291345aa7d43131a12fc6f64099
Exelles <aureliend@gmail.com>
2020-06-03 14:12:16 +0200

    Make it works!

--- index.js
+++ index.js
@@ -1,53 +1,80 @@
-const RSS = require('rss');
-const fs = require('fs');
+const RSS  = require('rss');
+const fs   = require('fs');
 const path = require('path');
 
-module.exports = function (api, options) {
-  api.beforeBuild(() => {
-    api.loadSource(actions => {
-      const feed = new RSS(options.feedOptions);
-      const {collection} = actions.getCollection(options.contentTypeName);
-      const dateField = options.dateField || 'date';
 
-      let collectionData = [...collection.data];
 
-      const collectionWithValidDates = collectionData.filter(node => !isNaN(new Date(node[dateField]).getTime()));
-      if (collectionWithValidDates.length === collectionData.length) {
-        collectionData.sort((nodeA, nodeB) => {
-          if (options.latest) {
-            return new Date(nodeB[dateField]).getTime() - new Date(nodeA[dateField]).getTime();
-          }
-
-          return new Date(nodeA[dateField]).getTime() - new Date(nodeB[dateField]).getTime();
-        });
-      }
-
-      if (options.maxItems) {
-        collectionData = collectionData.filter((item, index) => index < options.maxItems);
-      }
-
-      collectionData.forEach(item => {
-        feed.item(options.feedItemOptions(item));
-      });
-
-      const output = {
-        dir: './static',
-        name: 'rss.xml',
-        ...options.output
-      };
-
-      const outputPath = path.resolve(process.cwd(), output.dir);
-      const outputPathExists = fs.existsSync(outputPath);
-      const fileName = output.name.endsWith('.xml') ?
-        output.name :
-        `${output.name}.xml`;
-
-      if (outputPathExists) {
-        fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml());
-      } else {
-        fs.mkdirSync(outputPath);
-        fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), feed.xml());
-      }
-    });
+module.exports = function( api, options ) {  
+  if( ! options.output.subDir ) {
+    options.output.subDir = "/";
+  }
+  
+  if( ! options.output.name ) {
+    options.output.name = "rss.xml";
+  }
+  
+  api.loadSource(async store => {
+    this.store = store;
   });
-};
+  
+  api.afterBuild(async ({ queue, config }) => {    
+    const outputPath = path.join( config.outputDir, options.output.subDir );
+    const outputPathExists = fs.existsSync( outputPath );
+    const fileName = options.output.name.endsWith('.xml') ? options.output.name : `${options.output.name}.xml`;
+
+    console.log( `Generate RSS feed ${options.output.subDir}${fileName}` );
+    
+    const feed = new RSS( options.feedOptions );
+    const {collection} = this.store.getCollection( options.contentTypeName );
+    const dateField = options.dateField || 'date';
+    
+    let collectionData = [...collection.data];
+
+    const collectionWithValidDates = collectionData.filter( node => !isNaN(new Date(node[dateField]).getTime()) );
+    if( collectionWithValidDates.length === collectionData.length ) {
+      collectionData.sort((nodeA, nodeB) => {
+        if(options.latest) {
+          return new Date(nodeB[dateField]).getTime() - new Date(nodeA[dateField]).getTime();
+        }
+
+        return new Date(nodeA[dateField]).getTime() - new Date(nodeB[dateField]).getTime();
+      });
+    }
+
+    if( options.maxItems ) {
+      collectionData = collectionData.filter( (item, index) => index < options.maxItems );
+    }
+
+    collectionData.forEach(item => {
+      feed.item( options.feedItemOptions(item) );
+    });
+
+    if ( ! outputPathExists ) {
+      fs.mkdirSync( outputPath );
+    }
+    
+    fs.writeFileSync( path.resolve(outputPath, fileName), feed.xml() );
+  });
+}
+
+
+
+module.exports.defaultOptions = () => ({
+  contentTypeName: 'BlogPost',  // The typeName of the contentType you wish to generate your RSS file for.
+  feedOptions: {
+    title:     'Test',
+    feed_url:  'http://localhost:8080/rss.xml',
+    site_url:  'http://localhost:8080'
+  },
+  feedItemOptions: node => ({
+    title: node.title,
+    description: node.description,
+    url: 'http://localhost:8080/post/' + node.slug,
+    author: node.fields.author
+  }),
+  output: {
+    subDir:  '/',
+    name: 'rss.xml'
+  }
+})
+
